a38cf8d89a56f43b42cce4c523887ed5a769c133
Axi <jiedong9@gmail.com>
2019-09-26 07:32:54 +0800

    a

---
+++ array_learn.js
@@ -0,0 +1,2 @@
+let arr = [1, 2, 3, 4, 5]
+console.log(arr.findIndex(item => item % 2 === 0))
--- js_learn/js_demo.js
+++ js_learn/js_demo.js
@@ -1,7 +1,6 @@
-top:
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            if (i === 1 && j === 1) continue top;
-            console.log('i=' + i + ', j=' + j);
-        }
-    }
+// let i
+// for (let i = 0; i < 6; i++) {
+//     setTimeout(() => {
+//         console.log(i)
+//     }, 0)
+// }
